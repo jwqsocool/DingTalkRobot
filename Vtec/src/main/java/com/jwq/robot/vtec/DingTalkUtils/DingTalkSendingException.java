717717c9b9a699fadf6ab4ef73ae95d65544cdb6
Jiang Wanqi <1014349338@qq.com>
2022-07-20 14:34:50 +0800

    更改异常输出方法
    舍弃原先的默认方法，改为能够自定义组群的推送方法

--- Vtec/src/main/java/com/jwq/robot/vtec/DingTalkUtils/DingTalkSendingException.java
+++ Vtec/src/main/java/com/jwq/robot/vtec/DingTalkUtils/DingTalkSendingException.java
@@ -6,21 +6,21 @@ import com.dingtalk.api.DingTalkClient;
 import com.dingtalk.api.request.OapiRobotSendRequest;
 import com.dingtalk.api.response.OapiRobotSendResponse;
 import com.jwq.robot.vtec.config.DingTalkConfig;
+import com.jwq.robot.vtec.config.GroupConfig;
 import com.jwq.robot.vtec.param.DingTalkMsg;
 import com.taobao.api.ApiException;
 
 public class DingTalkSendingException {
 
 
-    public static void dingTalkSendingExceptionByMarkdown(Exception e) throws ApiException {
+    public static void dingTalkSendingExceptionByMarkdown(Exception e, GroupConfig group) throws ApiException {
         StackTraceElement stackTrace = e.getStackTrace()[0];
         String s = "**异常信息：**" + e.getMessage() +
                 "  \n  " + "  **所在文件：**  " + stackTrace.getFileName() +
                 "  \n  " + "  **报错行号：**  " + stackTrace.getLineNumber() +
                 "  \n  " + "  **相关方法：**  " + stackTrace.getMethodName() +
                 "  \n  " + "  **异常class：**  " + stackTrace.getClass() +
-                "  \n  " + "  **异常className：**  " + stackTrace.getClassName()
-                ;
+                "  \n  " + "  **异常className：**  " + stackTrace.getClassName();
         OapiRobotSendRequest request = new OapiRobotSendRequest();
         request.setMsgtype(DingTalkMsg.getMsgTypeMarkdown());
         OapiRobotSendRequest.Markdown markdown = new OapiRobotSendRequest.Markdown();
@@ -32,11 +32,11 @@ public class DingTalkSendingException {
         request.setAt(at);
 
         OapiRobotSendResponse response = new OapiRobotSendResponse();
-        try{
-            response = DingTalkConfig.client().execute(request);
-        }catch (Exception exception){
+        try {
+            response = new DefaultDingTalkClient(group.toString()).execute(request);
+        } catch (Exception exception) {
             exception.printStackTrace();
         }
-        System.out.println("钉钉机器人异常通知返回信息："+ JSON.toJSONString(response));
+        System.out.println("钉钉机器人异常通知返回信息：" + JSON.toJSONString(response));
     }
 }
